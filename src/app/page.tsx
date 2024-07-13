4c5af78ba9c261cd26fe2baba6d068c72392acd8
Margen Peterson <peterson.margen@gmail.com>
2024-07-13 15:33:36 +0300

    Css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { Suspense } from "react";
 import GemsList from "../components/gemList";
+import { SunIcon } from "@radix-ui/react-icons";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1 className='pb-4'>Praise the RNGesus</h1>
-        <Suspense fallback={<p>Loading...</p>}>
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-[#201A23]">
+      <div className="w-full max-w-md">
+        <h1 className='text-4xl font-bold text-center mb-8 text-[#FFBF1F]'>
+          <SunIcon className="inline-block w-6 h-6 mb-1 mx-2 text-[#FFBF1F]" />
+            Praise the RNGesus 
+          <SunIcon className="inline-block w-6 h-6 mb-1 mx-2 text-[#FFBF1F]" />
+        </h1>
+        <Suspense fallback={<p className="text-center text-gray-600 text-gray-400">Loading...</p>}>
           <GemsList />
         </Suspense>
       </div>
--- src/components/gemList.tsx
+++ src/components/gemList.tsx
@@ -1,21 +1,21 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import fetchGemData from '../api/fetchGemData';
 import { Button } from './ui/button';
-import { Separator } from './ui/separator';
 import gemData from '../data/combined_skill_gems.json';
+import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 
 export default function GemsList() {
   const [gems, setGems] = useState<string[]>([]);
   const [displayedGems, setDisplayedGems] = useState<(string | null)[]>([null, null, null]);
   const [locked, setLocked] = useState<boolean[]>([false, false, false]);
-
-/* This is from trade api, currently not in use
-  useEffect(() => {
-    fetchGemData().then(setGems);
-  }, []);*/
 
   useEffect(() => {
     const formattedGems = gemData.map(item => item.Name);
@@ -38,16 +38,37 @@ export default function GemsList() {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <Button onClick={handleReroll}>Reroll</Button>
-      <div className="gem-list">
+    <div className="relative flex flex-col items-center bg-[#5C4433] rounded-lg shadow-lg p-6">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <QuestionMarkCircledIcon className="absolute top-2 right-2 w-6 h-6 text-[#FFBF1F] cursor-pointer" />
+          </TooltipTrigger>
+          <TooltipContent side="top">
+            Click on the skill to keep it from being rolled
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <Button 
+        onClick={handleReroll}
+        className="mb-6 bg-[#FFBF1F] hover:bg-[#CC9200] text-[#261C15] font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
+      >
+        Reroll
+      </Button>
+      <div className="gem-list w-full">
         {displayedGems.map((gem, index) => (
-          <div key={index} onClick={() => toggleLock(index)} className="cursor-pointer text-base text-center mt-4 " style={{ color: locked[index] ? 'green' : 'black' }}>
-            {gem}
-            {index < 2 && displayedGems[index + 1] && <Separator className="my-4" />}
+          <div key={index} 
+               onClick={() => toggleLock(index)} 
+               className={`cursor-pointer text-xl text-center mt-4 p-3 rounded-md transition-colors duration-300 ease-in-out
+                          ${locked[index] 
+                            ? 'bg-[#CC9200] text-[#5C4433]' 
+                            : 'bg-[#DBCABD] text-gray-800'
+                          } hover:bg-[#9E7657]`}
+          >
+            {gem || 'Click Reroll'}
           </div>
         ))}
       </div>
     </div>
   );
-}
+}  
